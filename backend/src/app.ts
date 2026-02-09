b663af8930503789f76f79f89ab967e3f8740ff5
LaughingLab <dustinshoemake@hotmail.com>
2026-02-08 22:14:23 -0800

    Update CORS to use regex

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -44,8 +44,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:3000',
-        'https://youcancallmedustin.github.io',
-        'https://YouCanCallMeDustin.github.io'
+        /^https:\/\/youcancallmedustin\.github\.io\/?$/i
     ],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
